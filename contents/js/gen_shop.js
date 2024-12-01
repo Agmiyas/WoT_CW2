0f911e7946c0bea7fbd5ab1f17bfa09581bf7184
Башмаков Амир Саидович <asbashmakov@edu.hse.ru>
2024-12-01 19:52:50 +0300

    sort added

--- contents/js/gen_shop.js
+++ contents/js/gen_shop.js
@@ -1,4 +1,31 @@
-async function getResponce() {
+function sort(){
+    let price=document.getElementById("price")
+    let title=document.getElementById("title")
+    if(price.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        sortByPrice();
+    }
+
+    if(title.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        sortByTitle();
+    }
+}
+
+function comparator() { 
+    const nameA = a.title.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.title.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+    // Если имена одинаковые
+    return 0;
+}
+
+async function sortByTitle() {
     let responce = await fetch("./data/shop.json")
     let content = await responce.text()
     console.log(content)
@@ -12,17 +39,54 @@ async function getResponce() {
         console.log(content[key])
     }
 
+    content_title=content.sort((a, b) => comparator(a, b));
+    
     let node_for_insert = document.getElementById("node_for_insert")
     for (key in content) {
         node_for_insert.innerHTML += `
         <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
         <img style="width: 180px" class="align-self-center" src=${content[key].img}>
         <h5 class="card-title">${content[key].title}</h5>
-        <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
+        <p class="card-text">${content[key].description}.</p>
+        <p class="card-text"> Цена ${content[key].price} р.</p>
         <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
         <p class="card-text" >Заказать <input class="w-25" type="number" name="amount" value="0"></p>
         </li>
                 `
     }
 }
-getResponce()
+
+async function sortByPrice() {
+
+    let responce = await fetch("shop.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 9)
+    //content.sort()
+    console.log(content)
+    let key
+    /*for (key in content) {
+        console.log(content[key].id, content[key].title)
+        console.log(content[key])
+    }*/
+    content_price=content.sort((a, b) => a.price - b.price);
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    //node_for_insert.innerHTML='';
+    for (key in content_price) {
+                node_for_insert.innerHTML += `
+                <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 180px" class="align-self-center" src=${content[key].img}>
+                <h5 class="card-title">${content[key].title}</h5>
+                <p class="card-text">${content[key].description}. Цена ${content[key].price+content[key].price*0.2} р.</p>
+                <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
+                <p class="card-text" >!Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
+                </li>
+                        `
+            }
+
+}
+ 
+sort()
